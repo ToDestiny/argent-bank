2c6bcd45682e7498ff63fd7a55e6d9e93121c724
Anthony <anthonycolas.france@gmail.com>
2022-09-30 12:04:34 +0200

    Feature: LoginSlice created

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import loginReducer from '../features/login/loginSlice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    login: loginReducer,
+  },
 });
