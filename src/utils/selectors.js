6f9d8d1c8968a28b3bbe020e31a360c7713a198b
Anthony <anthonycolas.france@gmail.com>
2022-10-13 16:39:13 +0200

    Feature: SignUp Page OK

--- src/utils/selectors.js
+++
@@ -1,5 +0,0 @@
-const voidLogin = { status: 'void' };
-
-export const selectLogin = (emailUser, passwordUser) => (state) => {
-  return (state.login[emailUser] && state.login[passwordUser]) ?? voidLogin;
-};
